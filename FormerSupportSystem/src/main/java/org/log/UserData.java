4ead6484361bb2f907ea14dc89ec05314befc6f3
kansab <kansab.mohanraj@gmail.com>
2024-11-12 02:08:34 +0530

    Former Support System commited

---
+++ FormerSupportSystem/src/main/java/org/log/UserData.java
@@ -0,0 +1,28 @@
+package org.log;
+
+public class UserData {
+	
+	private  String username;
+	private Long phoneNum;
+	private String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Long getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(Long phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
